d28390b154b0767e42bb71510d81cf862fb01944
FrankQuiroz08 <95245517+FrankQuiroz08@users.noreply.github.com>
2022-09-28 19:39:07 -0500

    mapas_demarcacion

---
+++ styles/DEPARTAMENTOS_1_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_DEPARTAMENTOS_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "9.1px \'Berlin Sans FB Demi\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("DEPARTAMEN") !== null) {
+        labelText = String(feature.get("DEPARTAMEN"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,178,105,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
